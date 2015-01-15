1b914f693d3c803f939e902eed33bb7a2ce43b25
Phineas <phineas_huang@gemteks.com>
2015-01-15 14:56:30 +0800

    Initial Commit

---
+++ UIAutoTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UIAutoTest
+//
+//  Created by Gemtek_Phineas_Huang on 2015/1/15.
+//  Copyright (c) 2015年 SunXiaoShan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
